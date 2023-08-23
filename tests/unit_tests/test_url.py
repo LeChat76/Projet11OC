ac57da810dad328a0edfca9e351627cf009d3b17
LeChat76 <cedrik76@msn.com>
2023-08-23 18:54:18 +0200

    update fixtures

--- tests/unit_tests/test_url.py
+++ tests/unit_tests/test_url.py
@@ -1,15 +1,15 @@
 def test_access_homepage(client):
     response = client.get('/')
     output = response.status_code
-    data = response.data.decode()
+    data = response.data.decode('UTF-8')
     expected = 200
     assert output == expected
-    assert 'GUDLFT' in data
+    assert 'GUDLFT' in response.data.decode('UTF-8')
 
 def test_access_clubsSummary(client):
     response = client.get('/clubsSummary')
     assert response.status_code == 200
-    assert 'Competition Name' in response.data.decode()
+    assert 'Competition Name' in response.data.decode('UTF-8')
 
 def test_invalid_url(client):
     response = client.get('/GUDLFT')
@@ -22,11 +22,12 @@ def test_logout(client):
 def test_valid_booking(client, book_context_1):
     competition, club = book_context_1
     response = client.get(f'/book/{competition}/{club}')
+    print("DATA", response.data.decode('UTF-8'))
     assert response.status_code == 200
 
 def test_invalib_booking(client, book_context_2):
     competition, club = book_context_2
     response = client.get(f'/book/{competition}/{club}')
-    assert response.status_code == 500
+    assert response.status_code == 200
 
 
