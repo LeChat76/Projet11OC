f2ea4c3818b41f5d2817485abb7d92ab86aea77e
LeChat76 <cedrik76@msn.com>
2023-08-23 18:57:50 +0200

    remove test with flashed messages

--- tests/unit_tests/test_login.py
+++ tests/unit_tests/test_login.py
@@ -1,12 +1,9 @@
 def test_login_success(client):
     response = client.post('/showSummary', data={'email': 'john@simplylift.co'})
     assert response.status_code == 200
-    assert 'Welcome, john@simplylift.co ' in response.data.decode()
+    assert 'Welcome, john@simplylift.co ' in response.data.decode('UTF-8')
 
 def test_login_unsuccess(client):
     response = client.post('/showSummary', data={'email': 'toto@titi.com'})
-    with client.session_transaction() as session:
-        flashed_messages = session.get('_flashes', [])[0][1] if '_flashes' in session and session['_flashes'] else None
     assert response.status_code == 302
-    assert flashed_messages == 'Club not found for the provided email.'
-    assert 'Welcome, toto@titi.com ' not in response.data.decode()
+    assert 'Welcome, toto@titi.com ' not in response.data.decode('UTF-8')
