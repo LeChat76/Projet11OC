075cee25645479748ffdb45aa541365c03d98418
LeChat76 <cedrik76@msn.com>
2023-08-23 13:56:00 +0200

    functional test

---
+++ tests/func_tests/test_end_to_end.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# create firefox session
+browser = webdriver.Firefox()
+
+# open GUDLFT web site
+browser.get("http:/localhost:5000")
+assert 'GUDLFT Registration' == browser.title
+time.sleep(2)
+
+# search 'email' input field + fillin with value 'admin@irontemple.com'
+search_field = browser.find_element(By.NAME, 'email')
+search_field.send_keys('admin@irontemple.com')
+time.sleep(2)
+search_field.send_keys(Keys.RETURN)
+time.sleep(2)
+
+# click on link for purchase places in 'Fall Classic' competition
+link = browser.find_element(By.XPATH, '//a[@href="/book/Fall%20Classic/Iron%20Temple"]').click()
+
+# search 'places' input field
+search_field = browser.find_element(By.NAME, 'places')
+time.sleep(2)
+
+# fillin with value '3'
+search_field.send_keys('1')
+time.sleep(2)
+search_field.send_keys(Keys.RETURN)
+time.sleep(2)
+
+link = browser.find_element(By.XPATH, '//a[@href="/logout"]').click()
+time.sleep(2)
+
+# Close web browser
+browser.quit()
