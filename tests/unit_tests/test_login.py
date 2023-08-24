bf85ff608e0322bb561bf9f730eac239d256e8e4
LeChat76 <cedrik76@msn.com>
2023-08-24 16:16:03 +0200

    add marker for tests

--- tests/unit_tests/test_login.py
+++ tests/unit_tests/test_login.py
@@ -1,8 +1,13 @@
+import pytest
+
+
+@pytest.mark.unit_test
 def test_login_success(client):
     response = client.post('/showSummary', data={'email': 'john@simplylift.co'})
     assert response.status_code == 200
     assert 'Welcome, john@simplylift.co ' in response.data.decode('UTF-8')
 
+@pytest.mark.unit_test
 def test_login_unsuccess(client):
     response = client.post('/showSummary', data={'email': 'toto@titi.com'})
     assert response.status_code == 302
