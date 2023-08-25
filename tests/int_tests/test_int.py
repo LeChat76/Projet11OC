fa9bcab965baac5c3eb6cf0f2fb711401b34a03b
LeChat76 <cedrik76@msn.com>
2023-08-25 11:29:48 +0200

    debug int tests

--- tests/int_tests/test_int.py
+++ tests/int_tests/test_int.py
@@ -1,8 +1,10 @@
 import pytest
+import shutil
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from server import replace_data_files, foundClub, foundCompetition, loadClubs, loadCompetitions
 
-
-replace_data_files()
 
 @pytest.mark.int_test
 def test_login_and_logout(client):
@@ -20,49 +22,32 @@ def test_access_clubsSummary(client):
     assert 'Competition Name' in response.data.decode()
 
 @pytest.mark.int_test
-def test_purchasing_more_than_12_places_and_balance_keep_unchanged(client, purchase_context_4):
-    # purchase_context_4 = 'Iron Temple' purchase 13 places for 'Test 12' competition
-    replace_data_files()
-    club = purchase_context_4['club']
-    clubPointsBeforePurchase = int(foundClub(club)['points'])
-    competition = purchase_context_4['competition']
-    competitionNumberOfPlacesBeforePurchase = foundCompetition(competition)['numberOfPlaces']
-    response = client.post('purchasePlaces', data=purchase_context_4)
-    clubs_list = loadClubs()
-    competitions_list = loadCompetitions()
-    for element in clubs_list:
-        if element['name'] == club:
-            clubPointsAfterPurchase = element['points']
-            break
-    for element in competitions_list:
-        if element['name'] == competition:
-            competitionNmberOfPlacesAfterPurchase = element['numberOfPlaces']
-            break
-    assert response.status_code == 200
-    assert clubPointsBeforePurchase == clubPointsAfterPurchase
-    assert competitionNumberOfPlacesBeforePurchase == competitionNmberOfPlacesAfterPurchase
-
-@pytest.mark.int_test
 def test_purchasing_and_balance_update_checking(client, purchase_context_3):
     # purchase_context_3 = Iron Temple purchase 3 places for Fall Classic competition where remaing 5 places
-    club = purchase_context_3['club']
-    clubPointsBeforePurchase = int(foundClub(club)['points'])
-    print('\nclubPointsBeforePurchase1', clubPointsBeforePurchase)
+    replace_data_files()
+    club = foundClub(purchase_context_3['club'])
+    clubPointsBeforePurchase = club['points']
     nbPlacesToPurchase = int(purchase_context_3['places'])
-    competition = purchase_context_3['competition']
-    competitionNumberOfPlacesBeforePurchase = int(foundCompetition(competition)['numberOfPlaces'])
+    competition = foundCompetition(purchase_context_3['competition'])
+    competitionNumberOfPlacesBeforePurchase = competition['numberOfPlaces']
     response = client.post('purchasePlaces', data=purchase_context_3)
-    clubs_list = loadClubs()
-    competitions_list = loadCompetitions()
-    for element in clubs_list:
-        if element['name'] == club:
-            clubPointsAfterPurchase = element['points']
-            break
-    for element in competitions_list:
-        if element['name'] == competition:
-            competitionNmberOfPlacesAfterPurchase = element['numberOfPlaces']
-            break
+    clubPointsAfterPurchase = (foundClub(purchase_context_3['club']))['points']
+    competitionNmberOfPlacesAfterPurchase = (foundCompetition(purchase_context_3['competition']))['numberOfPlaces']
     assert response.status_code == 200
     assert clubPointsBeforePurchase - nbPlacesToPurchase == clubPointsAfterPurchase
     assert competitionNumberOfPlacesBeforePurchase - nbPlacesToPurchase == competitionNmberOfPlacesAfterPurchase
+
+@pytest.mark.int_test
+def test_purchasing_more_than_12_places_and_balance_keep_unchanged(client, purchase_context_4):
+    # purchase_context_4 = 'Iron Temple' purchase 13 places for 'Test 12' competition
     replace_data_files()
+    club = foundClub(purchase_context_4['club'])
+    clubPointsBeforePurchase = club['points']
+    competition = foundCompetition(purchase_context_4['competition'])
+    competitionNumberOfPlacesBeforePurchase = competition['numberOfPlaces']
+    response = client.post('purchasePlaces', data=purchase_context_4)
+    clubPointsAfterPurchase = (foundClub(purchase_context_4['club']))['points']
+    competitionNmberOfPlacesAfterPurchase = (foundCompetition(purchase_context_4['competition']))['numberOfPlaces']
+    assert response.status_code == 200
+    assert clubPointsBeforePurchase == clubPointsAfterPurchase
+    assert competitionNumberOfPlacesBeforePurchase == competitionNmberOfPlacesAfterPurchase
